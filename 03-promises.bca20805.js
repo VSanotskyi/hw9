53c2d3a58a3da7bb9618c719a0575b8b6e8abdd2
VSanotskyi <110067026+VSanotskyi@users.noreply.github.com>
2023-01-04 10:57:00 +0000

    Deploying to gh-pages from @ VSanotskyi/hw9@8657d7c852239af57633ca772bd91c0aaf594dfd 🚀

--- 03-promises.bca20805.js
+++
@@ -1,2 +0,0 @@
-({btn:document.querySelector(".form button"),firstDelay:document.querySelector('.form [name="delay" ]'),delayStep:document.querySelector('.form [name="step" ]'),amount:document.querySelector('.form [name="amount" ]')}).btn.addEventListener("click",(function(e){console.log(e),e.preventDefault(),console.log("hello")}));
-//# sourceMappingURL=03-promises.bca20805.js.map
