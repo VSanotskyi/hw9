8657d7c852239af57633ca772bd91c0aaf594dfd
VSanotskyi <110067026+VSanotskyi@users.noreply.github.com>
2023-01-04 12:56:23 +0200

    add

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,22 +1,50 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 const refs = {
+  form: document.querySelector('.form'),
   btn: document.querySelector('.form button'),
   firstDelay: document.querySelector('.form [name="delay" ]'),
   delayStep: document.querySelector('.form [name="step" ]'),
   amount: document.querySelector('.form [name="amount" ]'),
 };
 
-refs.btn.addEventListener('click', onClick);
-function onClick(evt) {
-  console.log(evt);
-  evt.preventDefault();
-  console.log('hello');
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
 }
 
-// function createPromise(position, delay) {
-//   const shouldResolve = Math.random() > 0.3;
-//   if (shouldResolve) {
-//     // Fulfill
-//   } else {
-//     // Reject
-//   }
-// }
+refs.btn.addEventListener('click', onCreatePromise);
+
+function onCreatePromise(evt) {
+  evt.preventDefault();
+
+  let delay = refs.firstDelay.value;
+  let step = refs.delayStep.value;
+  let position = 1;
+  const amount = refs.amount.value;
+
+  for (let i = 0; i < amount; i += 1) {
+    createPromise(position, delay)
+      .then(resolve => onResolve(resolve))
+      .catch(reject => onReject(reject));
+
+    delay = Number.parseInt(delay) + Number.parseInt(step);
+
+    position += 1;
+  }
+}
+
+function onResolve(res) {
+  Notify.success(res);
+}
+function onReject(rej) {
+  Notify.failure(rej);
+}
